1b7ec85dd9e37e51662a3a7817ef3973ccc6fbdc
carlos <carlos-yuan@qq.com>
2023-09-25 13:23:01 +0800

    修复cartime注入部分问题

--- util/gorm/callbacks.go
+++ util/gorm/callbacks.go
@@ -19,7 +19,43 @@ func RegisterCartimeCallbacks(db *gorm.DB) error {
 	if err != nil {
 		return err
 	}
+	err = db.Callback().Update().Replace("gorm:before_update", UpdateCallback)
+	if err != nil {
+		return err
+	}
+	err = db.Callback().Delete().Replace("gorm:before_delete", DeleteCallback)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+// UpdateCallback 自定义更新时间标记
+func UpdateCallback(db *gorm.DB) {
+	if db.Error == nil && db.Statement.Schema != nil && !db.Statement.SkipHooks {
+		updatedAtField := db.Statement.Schema.LookUpField("UpdatedAt")
+		switch db.Statement.ReflectValue.Kind() {
+		case reflect.Slice, reflect.Array:
+			for i := 0; i < db.Statement.ReflectValue.Len(); i++ {
+				v := reflect.Indirect(db.Statement.ReflectValue.Index(i))
+				if v.Kind() == reflect.Struct {
+					if updatedAtField != nil && updatedAtField.FieldType.Kind() == reflect.Int64 {
+						err := updatedAtField.Set(db.Statement.Context, db.Statement.ReflectValue.Index(i), cartime.NowToInt())
+						if err != nil {
+							db.AddError(err)
+						}
+					}
+				}
+			}
+		case reflect.Struct:
+			if updatedAtField != nil && updatedAtField.FieldType.Kind() == reflect.Int64 {
+				err := updatedAtField.Set(db.Statement.Context, db.Statement.ReflectValue, cartime.NowToInt())
+				if err != nil {
+					db.AddError(err)
+				}
+			}
+		}
+	}
 }
 
 // QueryCallback 自定义查询时间标记
@@ -32,6 +68,32 @@ func QueryCallback(db *gorm.DB) {
 			}
 		} else {
 			callbacks.Query(db)
+		}
+	}
+}
+
+// DeleteCallback 自定义删除时间标记
+func DeleteCallback(db *gorm.DB) {
+	if db.Error == nil && db.Statement.Schema != nil && !db.Statement.SkipHooks {
+		deleteField := db.Statement.Schema.LookUpField("DeletedAt")
+		if !db.Statement.Unscoped && deleteField != nil && deleteField.FieldType.Kind() == reflect.Int64 {
+			db.Statement.SQL.Grow(100)
+			//Soft Delete
+			if db.Statement.SQL.String() == "" {
+				db.Statement.AddClause(
+					clause.Set{{
+						Column: clause.Column{Name: deleteField.DBName},
+						Value:  cartime.NowToInt(),
+					}},
+				)
+				if conds := db.Statement.BuildCondition("`" + db.Statement.Table + "`" + ".`deleted_at`=0"); len(conds) > 0 {
+					db.Statement.AddClause(clause.Where{Exprs: conds})
+				}
+				db.Statement.AddClauseIfNotExists(clause.Update{})
+				db.Statement.BuildClauses = []string{"UPDATE", "SET", "WHERE"}
+			}
+		} else {
+			callbacks.BeforeDelete(db)
 		}
 	}
 }
